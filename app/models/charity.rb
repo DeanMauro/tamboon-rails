27e43c7102b13f1f7e5fd29e73ecdccbf232ece9
DeanMauro <selmerace@gmail.com>
2017-10-12 12:40:10 -0400

    Allow charity to be picked at random
    Added random button to view.
    Added methd to randomly select charity in model.

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -5,4 +5,9 @@ class Charity < ActiveRecord::Base
     new_total = total + amount
     update_attribute :total, new_total
   end
+
+  # Select a charity at random 
+  def self.random
+  	offset(rand(count)).first	# Not as random as PG Random() method, but is db-agnostic & will scale better
+  end
 end
